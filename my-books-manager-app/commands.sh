a70ccaa2279603c3acc10ecdaffda38634d8d638
StefanRM <stefanmaftei@gmail.com>
2020-01-14 18:38:30 +0200

    added script to easy run curl requests

---
+++ my-books-manager-app/commands.sh
@@ -0,0 +1,31 @@
+URL=0.0.0.0:6666
+
+case "$1" in
+	show)
+		curl -X GET http://$URL/books/show-books
+		echo
+		;;
+	add)
+		curl -X POST http://$URL/books/add-book -d '{"authors": "'"$2"'", "title": "'"$3"'"}' -H 'Content-Type: application/json'
+		echo
+		;;
+	update)
+		curl -X PUT http://$URL/books/update-book -d '{"id": "'"$2"'", "status": "'"$3"'"}' -H 'Content-Type: application/json'
+		echo
+		;;
+	status)
+		curl -X GET http://$URL/books/book-status?id=$2
+		echo
+		;;
+	delete)
+		curl -X DELETE http://$URL/books/delete-book -d '{"id": "'"$2"'"}' -H 'Content-Type: application/json'
+		echo
+		;;
+	search)
+		curl -X GET http://$URL/books/search-book-online?title=$2
+		echo
+		;;
+	*)
+		echo "Usage: ./$0 {show|add|update|delete|status|search}"
+		exit 1 
+esac
